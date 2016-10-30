be667c18b91e304c7c7bf63b6114dc5e4bac59ff
이화중 <hj_tl22@live.co.kr>
2016-10-31 05:18:42 +0900

    Test Examples

--- src/com/lhj/main/Main.java
+++ src/com/lhj/main/Main.java
@@ -1,35 +1,22 @@
 package com.lhj.main;
 
+import java.util.Arrays;
+
 public class Main {
 	
 	public static void main(String[] args) {
 		QuickSorter sorter = QuickSorter.getInstance();
-		Integer[] e1 = {8, 21, 93, 9, 28};
-		Integer[] e2 = {8, 21, 93, 9, 28};
+		Integer[] e1 = {8, 21, 93, 9, 28, 16, 34, 26, 54, 1};
+		Integer[] e2 = {8, 21, 93, 9, 28, 16, 34, 26, 54, 1};
 		
-		System.out.println(toString(e1));
+		System.out.println("------------- Recursion -------------");
+		System.out.println(Arrays.toString(e1));
 		sorter.sort(e1);
-		System.out.println(toString(e1));
+		System.out.println(Arrays.toString(e1));
 		
-		System.out.println("----------");
-		
-		System.out.println(toString(e2));
+		System.out.println("--- Tail Recursion Optimization ---");
+		System.out.println(Arrays.toString(e2));
 		sorter.sortTRO(e2);
-		System.out.println(toString(e2));
-	}
-	
-	private static <E> String toString(E[] e) {
-		if (e.length == 0) {
-			return "[]";
-		}
-		StringBuilder sb = new StringBuilder();
-		sb.append('[');
-		for (int i = 0;;) {
-			sb.append(e[i++]);
-			if (i >= e.length) {
-				return sb.append(']').toString();
-			}
-			sb.append(',').append(' ');
-		}
+		System.out.println(Arrays.toString(e2));
 	}
 }
